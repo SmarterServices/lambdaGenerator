d806918cc6150401184d5f267bd56a2db1eec5b6
jordan piepkow <jpiepkow@gmail.com>
2016-04-29 10:31:41 -0400

    added more setup to worker on yeoman prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,11 +13,90 @@ module.exports = yeoman.generators.Base.extend({
     ));
 
     var prompts = [{
-      type    : 'input',
-      name    : 'name',
-      message : 'Your lambda name',
-      default : this.appname // Default to current folder name
-    }];
+      type: 'input',
+      name: 'name',
+      message: 'Your lambda name',
+      default: this.appname // Default to current folder name
+    },
+      {
+        type: 'input',
+        name: 'env',
+        message: 'Your env for this worker',
+        default: 'dev' // Default to current folder name
+      },
+      {
+        type: 'input',
+        name: 'profile',
+        message: 'Your aws profile to publish your lambda',
+        default: 'Profile-name' // Default to current folder name
+      },
+      {
+        type: 'confirm',
+        name: 'alias',
+        message: 'Add alias to worker?',
+        default: true // Default to current folder name
+      }, {
+        when: function (res) {
+          return res.alias
+        },
+        type: 'input',
+        name: 'aliasName',
+        message: 'What is your alias name?',
+        default: 'Alias-name' // Default to current folder name
+      },
+      {
+        when: function (res) {
+          return res.alias
+        },
+        type: 'input',
+        name: 'version',
+        message: 'version to use with alias (current|$LATEST|[0-9])',
+        default: 'current' // Default to current folder name
+      },
+      {
+        when: function (res) {
+          return res.alias
+        },
+        type: 'input',
+        name: 'aliasDescription',
+        message: 'alias description',
+        default: 'alias description' // Default to current folder name
+      },
+      {
+        type: 'confirm',
+        name: 'iot',
+        message: 'Set up iot rule for worker?',
+        default: true // Default to current folder name
+      },
+      {
+        when: function (res) {
+          return res.iot
+        },
+        type: 'input',
+        name: 'RuleName',
+        message: 'What is your iot rule',
+        default: 'IotRuleName' // Default to current folder name
+      },
+      {
+        when: function (res) {
+          return res.iot
+        },
+        type: 'input',
+        name: 'sql',
+        message: 'Whats the sql for your rule?',
+        default: "SELECT * FROM '#' WHERE type='TYPE_TO_CATCH'" // Default to current folder name
+      },
+      {
+        when: function (res) {
+          return res.iot
+        },
+        type: 'input',
+        name: 'iotDescription',
+        message: 'What is your iot rule description',
+        default: "Iot rule description" // Default to current folder name
+      }
+    ];
+
 
     this.prompt(prompts, function (props) {
       this.props = props;
--- generators/app/templates/lambda-config.js
+++ generators/app/templates/lambda-config.js
@@ -1,28 +1,27 @@
 module.exports = {
   profile: 'lambda profile name',
+  <% if (iot) {%>
   iot: {
-    ruleName: 'iot rule name',
-    sql: "SELECT * FROM '#' WHERE type='TYPE_TO_CATCH'",
+    ruleName: '<%= RuleName %>',
+    sql: "<%= sql %>",
     overWrite:true ,
     ruleDisabled:true,
-    description:'rule description'
+    description:'<%= iotDescription %>'
   },
+ <% } %>
   region: 'lambda region',
-  version: 'function to use with alias (current|$LATEST|[0-9])',
-  alias: 'alias name',
-  aliasDescription: 'alias description',
-  env: 'function env ex. prod or dev',
+ <% if (alias) {%>
+  alias: '<%= name %>',
+  aliasDescription: '<%= name %>',
+  version: '<%=  %>',
+   <%}%>
+  env: '<%= name %>',
   publish: true,//will add a new version of function every time published if true. i false it will replace $LATEST
   handler: 'index.handler',
   role: 'arn:aws:iam::594833901287:role/lambda_basic_execution',
   functionName: '<%= name %>',
   timeout: 10,
-  memorySize: 128/*,
-   eventSource: {
-   EventSourceArn: <event source such as kinesis ARN>,
-   BatchSize: 200,
-   StartingPosition: "TRIM_HORIZON"
-   }*/
+  memorySize: 128
 };
 
 
